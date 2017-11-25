b3dfc99b03129bab47f70fc73e315288de61aa5d
Claudia Romano <cromano31415@gmail.com>
2017-11-25 08:43:27 -0600

    added templates, removed template home, modified app_js and index to include routing

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,23 @@
-angular.module('blocJams', []); //defines module named blocJams --this needs to be connected to the html tag in the index
+ (function() {
+     function config($stateProvider, $locationProvider) {
+     	 $locationProvider
+        	.html5Mode({
+            	 enabled: true, //hashbang URLS disabled
+            	 requireBase: false //unrelated
+         	});
+
+	    $stateProvider
+        	.state('landing', {
+            	url: '/',
+            	templateUrl: '/templates/landing.html'
+        	})
+        	.state('album', {	//another state named album
+             url: '/album',
+             templateUrl: '/templates/album.html'
+         });
+
+     }
+  angular
+         .module('blocJams', ['ui.router'])//module and dependency
+         .config(config); 
+ })();
