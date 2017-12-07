2c1be7c647fae2d32344d01d251a0aa0b0b48277
Claudia Romano <cromano31415@gmail.com>
2017-12-06 22:04:50 -0600

    assignment

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,12 +1,6 @@
 (function() {
      function AlbumCtrl(Fixtures) {
-     	  this.albumData = {};
-     	  this.songData = [];
   		  this.albumData = Fixtures.getAlbum();
-
-     for (var i=0; i < this.albumData[0].songs.length; i++) {
-	    this.songData.push(this.albumData[0].songs[i]);
-	   }
      }
  
      angular
